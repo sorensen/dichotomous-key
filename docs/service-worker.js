c8bb4333b46c2dff8bfa2971fd224f3a66dd601e
Beau Sorensen <mail@beausorensen.com>
2019-04-13 11:32:26 -0700

    Deploy issues

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.162d85133a2ef6c9f803cd34335e2b39.js"
+  "/dichotomous-key/precache-manifest.05873dc8a888558a49eba12446b5da4c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/dichotomous-key/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
